53f9f80336f38dc51f96770377a78f5178e37bce
Eduvale007 <eduvale007@gmail.com>
2025-11-23 13:27:36 -0300

    migrations deletadas

--- study_notes/study_notes/settings.py
+++ study_notes/study_notes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xd7x1baltzy_g(+4&co5lubfjsy$*vgx$a11-h#2n0^m+n2ygr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com']
 
+# 3. Garanta que o caminho dos estáticos esteja certo (no final do arquivo)
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Application definition
 
